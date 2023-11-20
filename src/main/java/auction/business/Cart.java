a2fbeb0689125abf6f54413f88caf5fb86dccf41
BDang029 <21110764@student.hcmute.edu.vn>
2023-11-20 10:23:27 +0700

    xong jpa

--- src/main/java/auction/business/Cart.java
+++ src/main/java/auction/business/Cart.java
@@ -1,33 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package auction.business;
-   
-import java.io.Serializable;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import static javax.persistence.FetchType.EAGER;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
-public class Cart implements Serializable {
-    
-    private Long cartId;
-    @OnetoMany(fetch=EAGER, cascade=CascadeType.PERSIST)  
-    private List<Product> cartItems;
-
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getCartId() {
-        return cartId;
-    }
-          
-    public List<Product> getcartItems() {
-        return cartItems;
-    }
-    public void setcartItems(List<Product> cartItems) {
-        this.cartItems = cartItems;
-    }
+/**
+ *
+ * @author Administrator
+ */
+public class Cart {
     
 }
